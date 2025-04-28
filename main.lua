73ff301f55e408d70908fdcb9f6dba969f2d5d9f
WaffleDevs <nsc.waffles@gmail.com>
2025-04-27 20:39:17 -0400

    cat tag!!!

--- main.lua
+++ main.lua
@@ -36,8 +36,66 @@ function generateTagUi()
                     major = G.HUD_tags[1] and G.HUD_tags[#G.HUD_tags] or G.ROOM_ATTACH
                 },
             }
-            done[tag.key] = G.HUD_tags[#G.HUD_tags]
+            done[tag.key] = {
+                HUD_tag = G.HUD_tags[#G.HUD_tags],
+                tag = tag
+            }
         end
-        G.GAME.tags[k].HUD_tag = done[tag.key]
+        G.GAME.tags[k].HUD_tag = done[tag.key].HUD_tag
+    end
+    -- Cryptid Cat Tag. 
+    local cat_ref = done["tag_cry_cat"]
+    if cat_ref and cat_ref.tag then
+        local cat_tag = cat_ref.tag
+        local logvalue = math.log(cat_tag.count,2)+1
+        if (logvalue == math.floor(logvalue)) and not (cat_tag.ability.level == logvalue) and not (logvalue == 1) then
+            cat_tag.ability.level = logvalue
+            local perc = (cat_tag.ability.level + 1)/10
+            if perc > 1 then perc = 1 end
+
+            local edition = G.P_CENTER_POOLS.Edition[1]
+            local j = 1
+            while j < cat_tag.ability.level + 1 do
+                for i = 2, #G.P_CENTER_POOLS.Edition do
+                    j = j + 1
+                    if j >= cat_tag.ability.level + 1 then
+                        edition = G.P_CENTER_POOLS.Edition[i]
+                        break
+                    end
+                end
+            end
+
+            G.E_MANAGER:add_event(Event({
+                delay = 0.0,
+                trigger = 'immediate',
+                func = (function()
+                    attention_text({
+                        text = ""..cat_tag.ability.level,
+                        colour = G.C.WHITE,
+                        scale = 1,
+                        hold = 0.3/G.SETTINGS.GAMESPEED,
+                        cover = cat_tag.HUD_tag,
+                        cover_colour = G.C.DARK_EDITION,
+                        align = 'cm',
+                    })
+                    play_sound('generic1', 0.8 + perc/2, 0.6)
+                    play_sound('multhit1', 0.9 + perc/2, 0.4)
+                    return true
+                end)
+            }))
+
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.2,
+                func = (function()
+                    cat_tag:juice_up(0.25, 0.1)
+                    cat_tag.ability.edshader = edition.shader
+                    print(edition.sound)
+                    play_sound(edition.sound.sound, (edition.sound.per or 1)*1.3, (edition.sound.vol or 0.25)*0.6)
+                    generateTagUi()
+                    return true
+                end)
+            }))
+        end
     end
 end 
